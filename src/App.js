5d071babfe94656df7b6ea5549e9234bd67a33a4
Konstantin Ivanov <konstiv23@gmail.com>
2022-01-14 23:07:17 +0300

    Added blurb

--- src/App.js
+++ src/App.js
@@ -8,6 +8,15 @@ function App() {
         <img src={instagramIcon} alt="Instagram icon" />
         <div className="instagram-handle__text">@asaldesign_uix</div>
       </a>
+      <div className="blurb blurb-container">
+        <h1 className="blurb__header">Panto Web Design #1</h1>
+        <h2 className="blurb__subtitle blurb__subtitle-countainer">
+          Landing Page Design for Furniture Website
+        </h2>
+        <a href="#" className="download-button">
+          Download now
+        </a>
+      </div>
     </div>
   );
 }
